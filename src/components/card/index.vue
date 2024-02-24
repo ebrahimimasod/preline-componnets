a14a4194c3b0c35362f3117969d0ba7fdbb9ae81
Shabnam ghanbari <shabghanbari44@gmail.com>
2024-02-24 17:51:17 +0330

    editpage2

--- src/components/card/index.vue
+++ src/components/card/index.vue
@@ -1,55 +1,72 @@
 <script setup>
- import {defineProps} from 'vue'
+import {defineProps} from 'vue'
 
 
-
-const props=defineProps({
-  title:{
-    type:String
+const props = defineProps({
+  title: {
+    type: String
   },
-  description:{
-    type:String
-
+  description: {
+    type: String
   },
-  link:{
-    type:String
+  link: {
+    type: String
   },
-  image:{
-    type:String
+  image: {
+    type: String
+  },
+  compact: {
+    type: Boolean,
+    default: false,
   }
 })
 
 </script>
 
 <template>
-    <div
-        class=" cardContainer bg-[url('https://blog.eterex.com/wp-content/uploads/2023/12/%D8%AE%D8%B1%DB%8C%D8%AF-%DA%A9%D8%AF-%DB%8C%D9%88-%D9%88%D9%88%DA%86%D8%B1-%D8%A8%D8%AF%D9%88%D9%86-%D8%A7%D8%AD%D8%B1%D8%A7%D8%B2-%D9%87%D9%88%DB%8C%D8%AA-%D9%88-%D8%AB%D8%A8%D8%AA-%D9%86%D8%A7%D9%85-%D9%88-%D8%A8%D8%AF%D9%88%D9%86-1%DA%A9%D8%A7%D8%B1%D9%85%D8%B2%D8%AF.jpg')]">
-      <h1 class="titleStyle">{{ title }}</h1>
-      <p class="descriptionStyle">
-{{description}}      </p>
-    </div>
+  <div
+      :style="{background:`url(${image})`}"
+      class="card-container">
 
+    <div class="w-full z-2  px-6 py-4 absolute bottom-0">
+
+      <h1 class="title-style mb-2 ">{{ title }}</h1>
+
+      <p class="description-style">
+        {{ description }}
+      </p>
+    </div>
+  </div>
 </template>
 
-
 <style>
-.cardContainer{
-  @apply w-3/6  h-56 flex flex-col  rounded-lg bg-gray-200  ;
-  background-position:center;
-  background-size: cover;
-  background-repeat: no-repeat
+.card-container {
+  @apply w-3/6  h-56 flex flex-col  rounded-lg bg-gray-200 relative ;
+  background-position: center !important;
+  background-size: cover !important;
+  background-repeat: no-repeat !important;
+  overflow: hidden;
+}
 
-}
-.titleStyle{
-  @apply text-base text-white text-right;
-position: absolute;
-}
-.descriptionStyle{
-  @apply text-sm text-white font-normal opacity-75 mt-5
-  p-5;
+
+.card-container::before {
+  content: "";
   position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+}
 
+.title-style {
+  @apply text-2xl text-white text-right font-bold;
+}
 
+.description-style {
+  @apply text-sm text-white text-center;
 }
 
 </style>
--- src/pages/Blog/index.vue
+++ src/pages/Blog/index.vue
@@ -2,11 +2,14 @@
 import Card from '@/components/card/index.vue'
 </script>
 <template>
-<div class="container !border-0 !shadow-none !mt-0">
+  <div class="container !border-0 !shadow-none !mt-0">
 
 
-  <Card title="سایت خرید ارز دیجیتال؛ آموزش نحوه خرید از سایت صرافی"
-        description="در این مطلب بهترین برنامه خرید ارز دیجیتال مطمئن و ساده در ایران را به شما معرفی می کنیم و روش کار با آن را تصویری آموزش می دهیم"/>
-</div>
+    <Card
+        title="سایت خرید ارز دیجیتال؛ آموزش نحوه خرید از سایت صرافی"
+        image="https://static.roocket.ir/images/cover/2023/12/1/9sZ46xZv44Bn4nZVUJWI4rOe78rwtMQv4sP4GkGe.jpg"
+        description="در این مطلب بهترین برنامه خرید ارز دیجیتال مطمئن و ساده در ایران را به شما معرفی می کنیم
+         و روش کار با آن را تصویری آموزش می دهیم"/>
+  </div>
 
 </template>
